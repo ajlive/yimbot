d2bfd26cc60f1aeb2a475b4da64116294c64ae6c
AJ Livingston <ajlive@longtall.city>
2021-10-23 20:37:25 -0600

    FaunaDB Hello world

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+// Import the driver
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	f "github.com/fauna/faunadb-go/v4/faunadb"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type HelloWorld struct {
+	Greeting string `fauna:"greeting"`
+	Name     string `fauna:"name"`
+}
+
+func run() error {
+	secret := os.Getenv("FAUNADB_SECRET")
+	if secret == "" {
+		return fmt.Errorf("The FAUNADB_SECRET environment variable is not set, exiting.")
+	}
+
+	endpoint := os.Getenv("FAUNADB_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "https://db.us.fauna.com/"
+	}
+
+	client := f.NewFaunaClient(secret, f.Endpoint(endpoint))
+
+	res, err := client.Query(f.Get(f.Ref(f.Collection("helloworld"), "313296063476793410")))
+	if err != nil {
+		return err
+	}
+
+	var hello HelloWorld
+	if err := res.At(f.ObjKey("data")).Get(&hello); err != nil {
+		return err
+	}
+
+	fmt.Printf("%v\n", strings.Join([]string{hello.Greeting, hello.Name}, ""))
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ajlive/yimbot
 
 go 1.17
+
+require (
+	github.com/fauna/faunadb-go/v4 v4.1.0
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
